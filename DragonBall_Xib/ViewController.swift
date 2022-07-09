423abe59a50e81c79bbca568d805592ebc68386f
sergio serrano <sergio.serrano1@vodafone.com>
2022-07-09 12:35:20 +0200

    Initial Commit

---
+++ DragonBall_Xib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragonBall_Xib
+//
+//  Created by sergio serrano on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DragonBall_XibUITests/DragonBall_XibUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DragonBall_XibUITestsLaunchTests.swift
+//  DragonBall_XibUITests
+//
+//  Created by sergio serrano on 9/7/22.
+//
+
+import XCTest
+
+class DragonBall_XibUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
